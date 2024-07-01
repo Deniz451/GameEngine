ef47ba316bec9a1c54b0447a21dc7aa108d65b42
Denis <114061026+Deniz451@users.noreply.github.com>
2024-07-01 14:10:45 +0200

    Add files via upload

---
+++ build/CMakeFiles/VulkanEngine.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VulkanEngine.dir/src/VulkanRenderer.cpp.obj"
+  "CMakeFiles/VulkanEngine.dir/src/VulkanRenderer.cpp.obj.d"
+  "CMakeFiles/VulkanEngine.dir/src/main.cpp.obj"
+  "CMakeFiles/VulkanEngine.dir/src/main.cpp.obj.d"
+  "VulkanEngine.exe"
+  "VulkanEngine.exe.manifest"
+  "VulkanEngine.pdb"
+  "libVulkanEngine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VulkanEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/VulkanEngine.dir/src/VulkanRenderer.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/VulkanEngine.dir/src/VulkanRenderer.cpp.obj: \
+ C:\Users\denis\Desktop\DenisResitko\Christos\src\VulkanRenderer.cpp
